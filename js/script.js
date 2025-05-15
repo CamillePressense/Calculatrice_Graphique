1b1155af85ac83e57588db103a458d7ccb5ca941
Pressense Fasani <camillefasani@gmail.com>
2025-05-15 10:10:13 +0200

    Etape 2: Display number clicked

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-const bootstrap = require('bootstrap');
+const AFFICHAGE = document.querySelector("#affichage");
+
+const UN = document.querySelector("#un");
+const DEUX = document.querySelector("#deux");
+const TROIS = document.querySelector("#trois");
+const QUATRE = document.querySelector("#quatre");
+const CINQ = document.querySelector("#cinq");
+const SIX = document.querySelector("#six");
+const SEPT = document.querySelector("#sept");
+const HUIT = document.querySelector("#huit");
+const NEUF = document.querySelector("#neuf");
+
+const CHIFFRES = [UN, DEUX, TROIS, QUATRE, CINQ, SIX, SEPT, HUIT, NEUF];
+
+CHIFFRES.forEach((element) => {
+ element.addEventListener("click", ()=>{
+    AFFICHAGE.value = element.value;
+    })
+})
+
+//Quand on clique sur + on récupère la valeur de affichage 
+//Quand on clique sur = on récupère la valeur de affichage et on l'additionne à la première
