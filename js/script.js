6efa048e229cf23545135fecf403a1b1a16451e8
Pressense Fasani <camillefasani@gmail.com>
2025-05-15 14:25:15 +0200

    Etape 3: Display result for a sum + reset button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const AFFICHAGE = document.querySelector("#affichage");
+const RESET = document.querySelector("#reset");
 
 const UN = document.querySelector("#un");
 const DEUX = document.querySelector("#deux");
@@ -14,9 +15,39 @@ const CHIFFRES = [UN, DEUX, TROIS, QUATRE, CINQ, SIX, SEPT, HUIT, NEUF];
 
 CHIFFRES.forEach((element) => {
  element.addEventListener("click", ()=>{
-    AFFICHAGE.value = element.value;
+    AFFICHAGE.value += element.value;
     })
 })
 
+RESET.addEventListener("click", reset);
+
+
+let premierNombre;
+let secondNombre;
+reset();
+
 //Quand on clique sur + on récupère la valeur de affichage 
+document.querySelector("#plus").addEventListener("click", () =>{
+    premierNombre = Number(AFFICHAGE.value);
+    AFFICHAGE.value = "";
+    })
+
 //Quand on clique sur = on récupère la valeur de affichage et on l'additionne à la première
+document.querySelector("#egal").addEventListener("click", () => {
+    const secondNombre = Number(AFFICHAGE.value);
+    const resultat = premierNombre + secondNombre;
+    AFFICHAGE.value = resultat;
+})
+
+
+function reset(){
+    AFFICHAGE.value = "";
+    premierNombre = "";
+    secondNombre = "";
+}
+
+
+/* function calculer(operation){
+    //si c'est un +
+    
+} */
