e4fd26581286680c7324fb18fecc485efb8d62fe
Pressense Fasani <camillefasani@gmail.com>
2025-05-16 12:13:59 +0200

    Etapes 3, 4, 5 + Refacto

--- js/script.js
+++ js/script.js
@@ -1,53 +1,40 @@
 const AFFICHAGE = document.querySelector("#affichage");
 const RESET = document.querySelector("#reset");
+const EGAL = document.querySelector("#egal");
 
-const UN = document.querySelector("#un");
-const DEUX = document.querySelector("#deux");
-const TROIS = document.querySelector("#trois");
-const QUATRE = document.querySelector("#quatre");
-const CINQ = document.querySelector("#cinq");
-const SIX = document.querySelector("#six");
-const SEPT = document.querySelector("#sept");
-const HUIT = document.querySelector("#huit");
-const NEUF = document.querySelector("#neuf");
+const CHIFFRES = document.querySelectorAll("#chiffre");
+const OPERATEURS = document.querySelectorAll("#operateur");
 
-const CHIFFRES = [UN, DEUX, TROIS, QUATRE, CINQ, SIX, SEPT, HUIT, NEUF];
+let resultat;
+reset();
 
 CHIFFRES.forEach((element) => {
  element.addEventListener("click", ()=>{
     AFFICHAGE.value += element.value;
+    resultat += element.value;
+    })
+})
+
+OPERATEURS.forEach((operateur) => {
+    operateur.addEventListener("click", ()=>{
+        resultat += operateur.value; 
+        AFFICHAGE.value = "";    
     })
 })
 
 RESET.addEventListener("click", reset);
 
-
-let premierNombre;
-let secondNombre;
-reset();
-
-//Quand on clique sur + on récupère la valeur de affichage 
-document.querySelector("#plus").addEventListener("click", () =>{
-    premierNombre = Number(AFFICHAGE.value);
-    AFFICHAGE.value = "";
-    })
-
-//Quand on clique sur = on récupère la valeur de affichage et on l'additionne à la première
-document.querySelector("#egal").addEventListener("click", () => {
-    const secondNombre = Number(AFFICHAGE.value);
-    const resultat = premierNombre + secondNombre;
-    AFFICHAGE.value = resultat;
+EGAL.addEventListener("click", () => {
+    const calcul = eval(resultat)
+    AFFICHAGE.value = calcul;
 })
-
 
 function reset(){
     AFFICHAGE.value = "";
-    premierNombre = "";
-    secondNombre = "";
+    resultat = "";
 }
 
 
-/* function calculer(operation){
-    //si c'est un +
-    
-} */
+
+
+
